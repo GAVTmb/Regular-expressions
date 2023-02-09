d4765fd6fab3b6cd9ea7717571c651b6e28083c1
Alexandr Galayura <first6886@mail.ru>
2023-02-09 21:10:31 +0300

    ...

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ with open("phonebook_raw.csv", 'r', encoding='utf-8') as f:
 
 
 def name_normalization(contacts_list):
-  new_contacts_list = []
+  name_normal_list = []
   search_phone = r'(8|\+7)\s*\(*(\d{1,3})\)*(\s|\-)*(\d{1,3})(\s|\-)*(\d{1,2})(\s|\-)*(\d{1,2})'
   pattern_phone = r'+7(\2)\4-\6-\8'
   search_phone_ext = r'(\(*)(доб.)(\s*)(\d+)(\)*)'
@@ -25,13 +25,35 @@ def name_normalization(contacts_list):
     resault_3 = re.sub(search_3, "", resault_2)
     resault_phone = re.sub(search_phone, pattern_phone, resault_3)
     resault_phone_ext = re.sub(search_phone_ext, pattern_phone_ext, resault_phone).split(",")
-    new_contacts_list.append(resault_phone_ext)
+    name_normal_list.append(resault_phone_ext)
 
+  return name_normal_list
+
+def delete_duplicates_contact(new_contacts_list):
+  resault = []
+  index = []
+  for list_1 in new_contacts_list:
+    ind = new_contacts_list.index(list_1)
+    for list_2 in new_contacts_list:
+      if list_1[:2] == list_2[:2] and list_1[2:] != list_2[2:]:
+        cum = list_1 + list_2
+        temp = []
+        for count in cum:
+          if count not in temp:
+            temp.append(count)
+        resault.append(temp)
+        index.append(ind)
+
+  index.reverse()
+  for c in index:
+    del (new_contacts_list[c])
+  new_contacts_list.append(resault[1])
+  new_contacts_list.append(resault[2])
   return new_contacts_list
 
-
-new_contacts_list = name_normalization(contacts_list)
-
+name_normal_list = name_normalization(contacts_list)
+new_contacts_list = delete_duplicates_contact(name_normal_list)
+# print(new_contacts_list)
 with open("phonebook.csv", "w", encoding='utf-8') as f:
   datawriter = csv.writer(f, delimiter=',')
   datawriter.writerows(new_contacts_list)
